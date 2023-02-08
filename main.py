da355f9bd9aa72cd064f378116203c560514ad62
brnkags <brnkagume@gmail.com>
2023-02-08 17:17:11 +0300

    Python backend

---
+++ main.py
@@ -0,0 +1,39 @@
+
+# comments are written using #symbol
+greeting = "Hello World"
+print(greeting)
+
+miles = 1000.0
+x = str(3)
+y = int(3)
+z = float(3)
+print(int(miles))
+xy = '5'
+
+print(type(xy))
+name1 = "Brian"
+Name1 = "Bri"
+fruits = ["apple", "cherry", "banana"]
+f1, f2, f3= fruits
+print(f1)
+print(f2)
+print(f3)
+fruit1, fruit2, fruit3 = 'banana', 'mango', 'orange'
+print(fruit1)
+print(fruit2)
+print(fruit3)
+
+a = 7
+b = 10
+c = a 
+a = b
+b = c
+print(a)
+print(b)
+g = 7
+k = 10
+
+g,k=k+0,g+0
+
+print(g)
+print(k)
---
+++ operator.py
@@ -0,0 +1,11 @@
+
+age = 19
+gender = 'female'
+
+if age <= 17:
+    print("Too young")
+elif age >= 18 and gender == 'male':
+    print("Can go in! But does not receive a free drink")
+elif age >= 18 and gender == 'female':
+    print("Can go in! receives a free drink, its ladies night")
+
